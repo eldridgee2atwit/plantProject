19548fec1f0ba7b10bb154cd8dac6297d665823d
eldridgee2atwit <eldridgee2@wit.edu>
2025-03-30 15:29:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,6 @@ def cleanup():
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True, host='0.0.0.0', port=5001)
+        app.run(debug=True, host='0.0.0.0', port=8080, use_reloader=False)
     finally:
         cleanup() 
