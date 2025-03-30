ea86cda56648f74a14bf59599e3b5262844e9bb2
eldridgee2atwit <eldridgee2@wit.edu>
2025-03-30 15:40:43 -0400

    try / catch for if the device isn't plugged in

--- src/readData.py
+++ src/readData.py
@@ -1,8 +1,24 @@
 import serial
-ser = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1)
+
 try:
-    while ser.is_open:
+    ser = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1)
+except:
+    ser = None
+
+def read_light():
+    if ser is None:
+        return 75  # Test value
+    try:
         received_data = ser.read_until('\n').decode('utf-8')
-        print(received_data)
-except KeyboardInterrupt:
-    ser.close()
+        return int(received_data)
+    except:
+        return 75  # Test value
+
+def read_moisture():
+    if ser is None:
+        return 60  # Test value
+    try:
+        received_data = ser.read_until('\n').decode('utf-8')
+        return int(received_data)
+    except:
+        return 60  # Test value
